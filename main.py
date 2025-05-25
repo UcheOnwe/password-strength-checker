b0f92cf1f7f6a412ca5a3d7294da2b16ecc5fb48
Uchechukwu Onwe <maxoutguide24@gmail.com>
2025-05-25 00:32:56 -0500

    Enhanced password checker
    * Added while loop to prompt user until a strong password is entered

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ def check_password_strength(password):
     digit_error = re.search(r"\d", password) is None
     uppercase_error = re.search(r"[A-Z]", password) is None
     lowercase_error = re.search(r"[a-z]", password) is None
-    symbol_error = re.search(r"[!@#$%^&*(),.?\":{}|<>]", password) is None
+    symbol_error = re.search(r"[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]", password) is None
     
-    if not (length_error or digit_error or uppercase_error or symbol_error):
-        return "Strong password"
-    else:
+    if (length_error or digit_error or uppercase_error or symbol_error):
         return "weak password - try adding numbers, caps, symbols, and at least 8 characters."
-
-password = input("Enter a password to check: ")
-print(check_password_strength(password))
+    else:
+        return "Strong Password"
+ 
+while True:
+    password = input("Enter a password to check: ")
+    result = check_password_strength(password)
+    print(result)
+    if result == "Strong Password":
+        break
